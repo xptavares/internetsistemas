7617bc1e48f0ab1dd5548d317b0ed8dc5a2d7615
Alexandre Tavares <xptavares@gmail.com>
2016-02-03 19:11:16 -0200

    scaffold course

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203210217) do
+ActiveRecord::Schema.define(version: 20160203210653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
